5330e64e5b6dcda75db8241fed944236b796e69e
Louis mermet <louismermet@MacBook-Air-de-Louis.local>
2026-04-27 18:28:37 +0200

    add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    { url: 'https://scpi-gouv.org', lastModified: new Date() },
+    { url: 'https://scpi-gouv.org/avantages', lastModified: new Date() },
+    { url: 'https://scpi-gouv.org/blog', lastModified: new Date() },
+    { url: 'https://scpi-gouv.org/comparaison', lastModified: new Date() },
+    { url: 'https://scpi-gouv.org/faq', lastModified: new Date() },
+    { url: 'https://scpi-gouv.org/fiscalite', lastModified: new Date() },
+    { url: 'https://scpi-gouv.org/revente', lastModified: new Date() },
+    { url: 'https://scpi-gouv.org/scpi-credit', lastModified: new Date() },
+  ];
+}
