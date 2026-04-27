b1344fe3191196e79fdc117eeb6544b83cfc30d1
Louis mermet <louismermet@MacBook-Air-de-Louis.local>
2026-04-27 19:28:40 +0200

    fix redirect loop

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.scpi-gouv.org",
-          },
-        ],
-        destination: "https://scpi-gouv.org/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
