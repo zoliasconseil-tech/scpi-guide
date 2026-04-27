87de02782cc1519033b7c137f71b6f14c770711b
Louis mermet <louismermet@MacBook-Air-de-Louis.local>
2026-04-27 19:26:24 +0200

    fix www redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.scpi-gouv.org",
+          },
+        ],
+        destination: "https://scpi-gouv.org/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
